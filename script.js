c07c250ff179f10b506c78b8146d5ff1837a56a2
Yee Shane <115919676+EkitaiKani@users.noreply.github.com>
2023-01-26 13:33:45 +0800

    Created supporting files

---
+++ script.js
@@ -0,0 +1,10 @@
+
+$("#carousel-slider").slick({
+    arrows: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    mobileFirst: true
+});
