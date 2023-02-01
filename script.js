bc5da5de8c45babcf355d5ccb7c59fc3717c3faa
vinhee <S10221118@connect.np.edu.sg>
2023-02-01 15:13:23 +0800

    Added sidenav and remove unwanted pages

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ $("#carousel-slider").slick({
     autoplaySpeed: 1500,
     mobileFirst: true
 });
+
+
+const searchInputSidenav = document.getElementById('search-input-sidenav');
+const sidenavOptions = document.querySelectorAll('#sidenav li .sidenav-link');
+
+searchInputSidenav.addEventListener('input', () => {
+  const filter = searchInputSidenav.value.toLowerCase();
+  showSidenavOptions();
+  const valueExist = !!filter.length;
+
+  if (valueExist) {
+    sidenavOptions.forEach((el) => {
+      const elText = el.textContent.trim().toLowerCase();
+      const isIncluded = elText.includes(filter);
+      if (!isIncluded) {
+        el.style.display = 'none';
+      }
+    });
+  }
+});
+
+const showSidenavOptions = () => {
+  sidenavOptions.forEach((el) => {
+    el.style.display = 'flex';
+  });
+};
